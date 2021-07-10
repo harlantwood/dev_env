1a1a2a8f7e33ae062e5b5441b2b6800efa463157
Harlan T Wood <code+harlan@harlantwood.net>
2021-07-09 20:25:09 -1000

    Setup new mac

---
+++ setup_new_mac.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+set -uxo pipefail
+# set -e
+
+xcode-select --install
+
+which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# show hidden files:
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+
+brew tap homebrew/cask
+
+brew install --cask iterm2
+
+brew install --cask visual-studio-code
+code_cmd="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+[ -r "$code_cmd" ] && ln -s "$code_cmd" "/usr/local/bin/code"
+
+brew install --cask flycut
+brew install --cask rectangle
+
+brew install zsh zsh-completions
+
+brew install golang
+export GOPATH="${HOME}/.go"
+export GOROOT="$(brew --prefix golang)/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+mkdir -p "${GOPATH}"
+mkdir -p "${GOPATH}/src/github.com"
+go get -u github.com/justjanne/powerline-go
+
+brew install yarn
+
+brew install git
+brew install hub
+
+brew tap git-duet/tap
+brew install git-duet
+
+brew tap heroku/brew
+brew install heroku
+heroku autocomplete --refresh-cache
+
+brew install rbenv
+rbenv init
+
+brew install telnet
+brew install --cask multipass
+
+brew install cmake
+brew install --cask chromedriver
+brew install lnav
+brew install postgresql@12
+brew services start postgresql@12
+brew install memcached
+brew services start memcached
+brew install openvpn
+brew install --cask ngrok
+brew install shellcheck
+brew install nginx
+brew services start nginx
+brew install md5sha1sum
+
+# you could check if a more recent version exists here:
+# https://github.com/nvm-sh/nvm/tags
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash

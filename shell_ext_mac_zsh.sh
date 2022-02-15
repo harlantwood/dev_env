6370d9d9a5804816142479350ba13f75f912a32c
Harlan T Wood <code+harlan@harlantwood.net>
2022-02-15 12:14:13 -1000

    VS Code, aliases

--- shell_ext_mac_zsh.sh
+++ shell_ext_mac_zsh.sh
@@ -232,16 +232,16 @@ export PATH="/usr/local/opt/postgresql@12/bin:$PATH"
 # on all nine background colors (default + 8 escapes).
 # from http://bitmote.com/index.php?post/2012/11/19/Using-ANSI-Color-Codes-to-Colorize-Your-Bash-Prompt-on-Linux
 #
-# function colors {
-#   T='Fox'   # The test text
-#   echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m";
-#   for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m';
-#     do FG=${FGs// /}
-#     echo -en " $FGs \033[$FG  $T  "
-#     for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
-#       do echo -en "$EINS \033[$FG\033[$BG  $T \033[0m\033[$BG \033[0m";
-#     done
-#     echo;
-#   done
-#   echo
-# }
+function colors {
+  T='XXX'
+  echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m";
+  for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m';
+    do FG=${FGs// /}
+    echo -en " $FGs \033[$FG  $T  "
+    for BG in 40m 41m 42m 43m 44m 45m 46m 47m;
+      do echo -en "$EINS \033[$FG\033[$BG  $T \033[0m\033[$BG \033[0m";
+    done
+    echo;
+  done
+  echo
+}
